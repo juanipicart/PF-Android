0548a06dcd6098c641562a1c6348492617c72600
Damian <d-bozz@hotmail.com>
2020-09-08 17:11:48 -0300

    fix

--- app/src/main/java/com/example/pf_androidapp/DetalleObservacion.java
+++ app/src/main/java/com/example/pf_androidapp/DetalleObservacion.java
@@ -2,14 +2,12 @@ package com.example.pf_androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class DetalleObservacion extends AppCompatActivity {
 
     private Button btnAceptar;
     private EditText txtCodigo;
@@ -20,11 +18,16 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtLatitud;
     private EditText txtAltitud;
 
+    private EditText txtComboFen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detalle_observacion);
 
+        setContentView(R.layout.activity_detalle_observacion);
+
+        txtComboFen = (EditText) ComboFenomeno.getText();
         txtCodigo = (EditText)findViewById(R.id.txtCodigo);
         btnAceptar = (Button)findViewById(R.id.btnAceptar);
         txtDescripcion = (EditText)findViewById(R.id.txtdescripcion);
@@ -36,32 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        btnAceptar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, DetalleObservacion.class);
+        Bundle bundle = this.getIntent().getExtras();
 
-                Bundle b = new Bundle();
-                b.putString("CODIGO", txtCodigo.getText().toString());
-                b.putString("DESCRIPCION", txtDescripcion.getText().toString());
-                b.putString("FENOMENO", ComboFenomeno.getSelectedItem().toString());
-                b.putString("LOCALIDAD", ComboLocalidad.getSelectedItem().toString());
-                b.putString("ALTITUD", txtAltitud.getText().toString());
-                b.putString("LATITUD", txtLatitud.getText().toString());
-                b.putString("LONGITUD", txtLongitud.getText().toString());
-
-                intent.putExtras(b);
-
-                startActivity(intent);
-            }
-        });
-
-
-
-
+        txtCodigo.setText("Codigo:" + bundle.getString("CODIGO"));
+        txtDescripcion.setText("Descripcion: " + bundle.getString("DESCRIPCION"));
+        txtDescripcion.setText("Descripcion: " + bundle.getString("DESCRIPCION"));
+        txtDescripcion.setText("Descripcion: " + bundle.getString("DESCRIPCION"));
     }
 
-
-
+    private int getSelectedItem(Spinner spinner, String value){
+        int index = 0;
+        for (int i=0; i<spinner.getCount();i++) {
+            if (spinner.getItemAtPosition(i).equals(value))
+                index = i;
+        }
+    }
 
 }
