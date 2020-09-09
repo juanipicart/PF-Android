9ce7971bb25c5a13d9192ebeed317a93470535d2
Juan <juanipicart@gmail.com>
2020-09-09 20:47:30 -0300

    Agrego campo fecha

--- app/src/main/java/com/example/pf_androidapp/MainActivity.java
+++ app/src/main/java/com/example/pf_androidapp/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.pf_androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private Button btnAceptar;
     private EditText txtCodigo;
@@ -19,12 +32,43 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtLongitud;
     private EditText txtLatitud;
     private EditText txtAltitud;
+    private TextView fechaCreacion;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fechaCreacion = (TextView) findViewById(R.id.fechaCreacion);
 
+        fechaCreacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: dd/mm/yyyy: " + day + "/" + month + "/" + year);
+                String date = day + "/" + month + "/" + year;
+                fechaCreacion.setText(date);
+
+            }
+        };
        /* txtCodigo = (EditText)findViewById(R.id.txtCodigo);
         btnAceptar = (Button)findViewById(R.id.btnAceptar);
         txtDescripcion = (EditText)findViewById(R.id.txtdescripcion);
@@ -56,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 */
-
 
 
     }
